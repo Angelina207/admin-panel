ecebd06d04974f234324db2bb42aaa8e26a60059
Angelina <kuzmenkoa022@gmail.com>
2022-09-07 14:50:00 +0300

    carousel-markup

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import * as flsFunctions from "./modules/functions.js";
 import * as headerFunc from './modules/header.js';
 import * as filterFunc from './modules/filter.js'
 import * as asideFunc from './modules/aside.js'
+import * as caruoselFunc from './modules/carousel.js'
 
 flsFunctions.isWebp();
 headerFunc.header();
 filterFunc.filter();
 asideFunc.aside();
+caruoselFunc.carousel();
 
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
---
+++ src/js/modules/carousel.js
@@ -0,0 +1,8 @@
+export function carousel() {
+    const slider = document.querySelector('.tab-content');
+    const cards = document.querySelectorAll('.carousel__card');
+    const btnLeft = document.querySelector('.btn-left');
+    const btnright = document.querySelector('.btn-right');
+
+    console.log(slider.style.width)    
+}
