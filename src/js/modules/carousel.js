57dc598d88bbb8eb77902a27a2dab3ea770d7f25
Angelina <kuzmenkoa022@gmail.com>
2022-09-08 19:20:03 +0300

    tabs

--- src/js/modules/carousel.js
+++ src/js/modules/carousel.js
@@ -1,8 +1,15 @@
 export function carousel() {
-    const slider = document.querySelector('.tab-content');
+    const tabs = document.querySelector('.tab__tab-btn').addEventListener('click', showCurrentTab)
+    const slider = document.querySelectorAll('.tab-content');
     const cards = document.querySelectorAll('.carousel__card');
-    const btnLeft = document.querySelector('.btn-left');
-    const btnright = document.querySelector('.btn-right');
 
-    console.log(slider.style.width)    
+    function showCurrentTab(event) {
+        if (event.target.className === 'tab-btn-swich') {
+            const tabAttr = parseInt(event.target.dataset.tab);
+
+            for (let i = 0; i < slider.length; i++) {
+                tabAttr === i ? slider[i].style.display = 'flex' : slider[i].style.display = 'none';
+            }
+        }
+    }
 }
