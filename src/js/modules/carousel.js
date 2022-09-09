daa6bc8b00e88777b2138650cad430ea19cbc69d
Angelina <kuzmenkoa022@gmail.com>
2022-09-09 19:33:10 +0300

    contacts

--- src/js/modules/carousel.js
+++ src/js/modules/carousel.js
@@ -1,7 +1,11 @@
 export function carousel() {
-    const tabs = document.querySelector('.tab__tab-btn').addEventListener('click', showCurrentTab)
+    // Tabs
+    document.querySelector('.tab__tab-btn').addEventListener('click', showCurrentTab);
+    document.querySelector('.tab__tab-btn').addEventListener('click', aciveBtn);
+    const tabInnerBtn = document.querySelectorAll('.tab-btn-swich');
+    const wave = document.querySelector('.tab__wave');
     const slider = document.querySelectorAll('.tab-content');
-    const cards = document.querySelectorAll('.carousel__card');
+    let currentTab;
 
     function showCurrentTab(event) {
         if (event.target.className === 'tab-btn-swich') {
@@ -11,5 +15,22 @@ export function carousel() {
                 tabAttr === i ? slider[i].style.display = 'flex' : slider[i].style.display = 'none';
             }
         }
+    }
+
+    function aciveBtn(event) {
+        if (currentTab = event.target.dataset.tab) {
+
+            for (let i = 0; i < tabInnerBtn.length; i++) {
+                currentTab == i ? tabInnerBtn[i].classList.add('tab-is-active') : tabInnerBtn[i].classList.remove('tab-is-active');
+            }   
+
+            if (currentTab == '0') {
+                wave.style.top = '0%';
+            } else if (currentTab == '1') {
+                wave.style.top = 'calc(50% - 68px)';
+            } else if (currentTab == '2') {
+                wave.style.top = '66%';
+            }
+        } 
     }
 }
