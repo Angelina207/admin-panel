270d90d8d3f8c0392b3a2c8a580f0f190d185a85
Angelina <kuzmenkoa022@gmail.com>
2022-10-15 16:15:52 +0300

    gallery

--- src/js/modules/gallery.js
+++ src/js/modules/gallery.js
@@ -1,28 +1,35 @@
 export function gallery() {
     const gallery = document.querySelector('.gallery-wrapp');
     const galleryWrapp = document.querySelector('.gallery-img-wrapp');
-    const galleryImg = document.querySelectorAll('.photo-list-img');
-    const galleryBtnLeft = document.querySelector('#left');
-    const galleryBtnRight = document.querySelector('#right');
 
-    document.querySelector('.photo-list').addEventListener('click', openGallery);
-    document.querySelector('.gallery-close-btn').addEventListener('click', closeGallery);
+    function openGallery() {  
+        document.querySelector('.photo-list').addEventListener('click', event => {
+            let imgAttr;
+            const imgElem = event.target;           
 
-    function openGallery(event) {
-        const currentImg = event.target.className === 'photo-list-img';
-        
-        if (currentImg) {
-            gallery.style.opacity = '1';
-            gallery.style.zIndex = '10';
-            galleryWrapp.style.transform = 'scale(1)';
-            galleryWrapp.style.opacity = '1';
-        }
+            if (imgElem) {
+                imgAttr = event.target.getAttribute('src');
+            }
+
+            if (imgElem) {
+                gallery.style.opacity = '1';
+                gallery.style.zIndex = '10';
+                galleryWrapp.style.transform = 'scale(1)';
+                galleryWrapp.style.opacity = '1';
+            }
+
+            document.querySelector('.gallery-img').src = imgAttr;
+        })
     }
+    openGallery()
 
     function closeGallery() {
+        document.querySelector('.gallery-close-btn').addEventListener('click', () => {
             gallery.style.opacity = '0';
             gallery.style.zIndex = '-1';
             galleryWrapp.style.transform = 'scale(2)';
             galleryWrapp.style.opacity = '0';
+        });
     }
+    closeGallery()
 }
