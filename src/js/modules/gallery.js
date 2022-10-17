e4128e7570dd5abc9a11ba6d7a283520a768c884
Angelina <kuzmenkoa022@gmail.com>
2022-10-17 21:16:42 +0300

    gallery

--- src/js/modules/gallery.js
+++ src/js/modules/gallery.js
@@ -1,28 +1,40 @@
 export function gallery() {
     const gallery = document.querySelector('.gallery-wrapp');
     const galleryWrapp = document.querySelector('.gallery-img-wrapp');
+    document.querySelector('.photo-list').addEventListener('click', openGallery)
 
-    function openGallery() {  
-        document.querySelector('.photo-list').addEventListener('click', event => {
-            let imgAttr;
-            const imgElem = event.target;           
+    function openGallery(event) {  
+        document.querySelector('#rigth').addEventListener('click', scrollRight);
+        document.querySelector('#left').addEventListener('click', scrollLeft);
+        let imgAttr;
+        let currSrc;
+        const imgElem = event.target;      
 
-            if (imgElem) {
-                imgAttr = event.target.getAttribute('src');
-            }
+        if (imgElem) {
+            imgAttr = event.target.getAttribute('src');
+        }
+        currSrc = document.querySelector('.gallery-img').src = imgAttr;
 
+        function scrollRight() {
+            const images = document.querySelectorAll('.photo-list-img');
+
+        }
+
+        function scrollLeft() {
+            
+        }
+
+        function open() {
             if (imgElem) {
                 gallery.style.opacity = '1';
                 gallery.style.zIndex = '10';
                 galleryWrapp.style.transform = 'scale(1)';
                 galleryWrapp.style.opacity = '1';
             }
-
-            document.querySelector('.gallery-img').src = imgAttr;
-        })
+        }
+        open()
     }
-    openGallery()
-
+ 
     function closeGallery() {
         document.querySelector('.gallery-close-btn').addEventListener('click', () => {
             gallery.style.opacity = '0';
@@ -31,7 +43,5 @@ export function gallery() {
             galleryWrapp.style.opacity = '0';
         });
     }
-    closeGallery()
-
-    
+    closeGallery()    
 }
